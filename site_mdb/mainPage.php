570b60515d4e26014b8826b489efc316213632c5
4NUB1Ss2 <soprasalvarf@gmail.com>
2025-06-12 01:39:37 -0300

    clicks

--- site_mdb/mainPage.php
+++ site_mdb/mainPage.php
@@ -131,11 +131,11 @@ while($tbl=mysqli_fetch_array($result))
 	$img = $tbl["image"];
   $clicks = $tbl["clicks"]+1;
 	
-
-
-
+  
   echo "<div class='col-sm-3'>";
     echo "<div class='card'>"; 
+    echo "<form action='./php/card.php' method='post'>";
+    echo "<input type='hidden' name='game_id' value='$id'>";
   echo "<div class='bg-image hover-overlay' data-mdb-ripple-init data-mdb-ripple-color='light'>";
     echo "<img src='https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp' class='img-fluid'/>";
     echo "<a href='#!'>";
@@ -146,13 +146,16 @@ while($tbl=mysqli_fetch_array($result))
     echo "<h5 class='card-title text-center'> $name</h5>";
     echo "<p class='card-text'> $desc</p>";
     
-    echo "<a href=$link class='btn btn-primary align-items-center' data-mdb-ripple-init>Baixar</a>";
-  echo "</div>";
+    echo "<button type='submit' class='btn btn-primary align-items-center' data-mdb-ripple-init>Baixar</button>";
+  echo "</form>";
+    echo "</div>";
   echo "</div>"; 
   echo "</div>";
+  echo "<br>";
+  echo "<br>";
+  echo "<br>";
+  
 
-  $click = "UPDATE games set clicks='$clicks' where id='$id'";
-  $click_query = mysqli_query($conn, $click) or die("ERRO");
 
 }
 ?>
@@ -165,67 +168,50 @@ while($tbl=mysqli_fetch_array($result))
 <div class="container-lg">
   <h1>Mais Baixados</h1>
 <div class="row">
-  <div class="col-sm-3">
-    <div class="card">
-  <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-    <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" class="img-fluid"/>
-    <a href="#!">
-      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-    </a>
-  </div>
-  <div class="card-body">
-    <h5 class="card-title text-center">jogo 1</h5>
-    <p class="card-text">Uma breve descrição do jogo, contando a história ou as características do jogo</p>
-    <a href="#!" class="btn btn-primary align-items-center" data-mdb-ripple-init>Baixar</a>
-  </div>
-  </div>  
-  </div>
+  <?php
 
-  <div class="col-sm-3">
-    <div class="card">
-  <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-    <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" class="img-fluid"/>
-    <a href="#!">
-      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-    </a>
-  </div>
-  <div class="card-body">
-    <h5 class="card-title text-center">jogo 2</h5>
-    <p class="card-text">Uma breve descrição do jogo, contando a história ou as características do jogo</p>
-    <a href="#!" class="btn btn-primary align-items-center" data-mdb-ripple-init>Baixar</a>
-  </div>
-  </div>
-  </div>
-  <div class="col-sm-3">
-    <div class="card">
-  <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-    <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" class="img-fluid"/>
-    <a href="#!">
-      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-    </a>
-  </div>
-  <div class="card-body">
-    <h5 class="card-title text-center">jogo 3</h5>
-    <p class="card-text">Uma breve descrição do jogo, contando a história ou as características do jogo</p>
-    <a href="#!" class="btn btn-primary align-items-center" data-mdb-ripple-init>Baixar</a>
-  </div>
-  </div>
-  </div>
-  <div class="col-sm-3">
-    <div class="card">
-  <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-    <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" class="img-fluid"/>
-    <a href="#!">
-      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-    </a>
-  </div>
-  <div class="card-body">
-    <h5 class="card-title text-center">jogo 4</h5>
-    <p class="card-text">Uma breve descrição do jogo, contando a história ou as características do jogo</p>
-    <a href="#!" class="btn btn-primary align-items-center" data-mdb-ripple-init>Baixar</a>
-  </div>
-  </div>
-  </div>
+
+$clicados = "SELECT * FROM games ORDER BY clicks DESC LIMIT 4";
+$query2 = mysqli_query($conn, $clicados) or die ("Erro");
+$result2=mysqli_query($conn,$clicados);
+
+while($tbl2=mysqli_fetch_array($result2))
+{
+	
+  $id = $tbl2["id"];
+  $link = $tbl2["link"];
+	$name = $tbl2["name"];
+	$desc = $tbl2["description"];
+	$img = $tbl2["image"];
+  $clicks = $tbl2["clicks"]+1;
+
+  echo "<div class='col-sm-3'>";
+    echo "<div class='card'>"; 
+    echo "<form action='./php/card.php' method='post'>";
+    echo "<input type='hidden' name='game_id' value='$id'>";
+  echo "<div class='bg-image hover-overlay' data-mdb-ripple-init data-mdb-ripple-color='light'>";
+    echo "<img src='https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp' class='img-fluid'/>";
+    echo "<a href='#!'>";
+     echo "<div class='mask' style='background-color: rgba(251, 251, 251, 0.15);'></div>";
+    echo "</a>";
+  echo "</div>";
+  echo "<div class='card-body'>";
+    echo "<h5 class='card-title text-center'> $name</h5>";
+    echo "<p class='card-text'> $desc</p>";
+    
+    echo "<button type='submit' class='btn btn-primary align-items-center' data-mdb-ripple-init>Baixar</button>";
+  echo "</form>";
+    echo "</div>";
+  echo "</div>"; 
+  echo "</div>";
+  echo "<br>";
+  echo "<br>";
+  echo "<br>";
+  
+
+
+}
+?>
 </div>
 <br>
 <br><br>
--- site_mdb/php/card.php
+++ site_mdb/php/card.php
@@ -1,18 +1,24 @@
 <?php
+
 include_once("conexao.php");
 
-$name;
-$description;
-$link;
+if (isset($_POST['game_id'])) {
+    $id = intval($_POST['game_id']);
+    // Incrementa o contador
+    $update = "UPDATE games SET clicks = clicks + 1 WHERE id = $id";
+    mysqli_query($conn, $update);
 
+    // Busca o link do jogo
+    $result = mysqli_query($conn, "SELECT link FROM games WHERE id = $id");
+    $row = mysqli_fetch_assoc($result);
+    $link = $row['link'];
 
-$sql = "SELECT * FROM games ORDER BY id DESC LIMIT 4";
-$query = mysqli_query($conn, $sql) or die ("Erro");
-
-
-
-
-
-
-
+    // Redireciona para o link do jogo
+    header("Location: $link");
+    exit();
+} else {
+    // Se não houver ID, volta para a página principal
+    header("Location: ../mainPage.php");
+    exit();
+}
 ?>
